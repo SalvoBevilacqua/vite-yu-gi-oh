61a1c35c5b18de92e28b54cafb86d5e96a910a6e
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-27 19:08:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0").then((resp) => {
+      this.list = resp.data.data;
+      return this.list;
+    })
+  }
+}
+</script>
+
+<template>
+  <h1 class="text-center">prova</h1>
+  <!-- <AppHeader /> -->
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+    <header class="d-flex p-3">
+        <img src="../img/logo.png" alt="logo">
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+</style>
